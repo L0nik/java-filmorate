5538c335f7a0c965e0fdbcb4538dddc18d6ae03a
l0nik <l0nik@mail.ru>
2026-02-19 14:28:16 +0300

    первый тест

--- src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
@@ -1,13 +1,45 @@
 package ru.yandex.practicum.filmorate;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 class FilmorateApplicationTests {
+
+    private static final String BASE_URL = "http://localhost:8080/films";
+    private static HttpClient client;
+
+    @BeforeAll
+    static void setup() {
+        client = HttpClient.newHttpClient();
+    }
 
 	@Test
 	void contextLoads() {
 	}
+
+    @Test
+    @DisplayName("GET /films -> 200 OK")
+    void shouldReturn200WhenGetFilms() throws IOException, InterruptedException
+    {
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(BASE_URL))
+                .timeout(Duration.ofSeconds(5))
+                .GET()
+                .build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        assertEquals(200, response.statusCode());
+    }
 
 }
