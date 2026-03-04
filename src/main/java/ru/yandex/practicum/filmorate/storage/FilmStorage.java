7812c30a5df9769d313f85be9c4435772e224e5e
l0nik <l0nik@mail.ru>
2026-03-04 14:38:45 +0300

    исправление ошибок

--- src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
@@ -7,7 +7,10 @@ import java.util.Collection;
 public interface FilmStorage {
 
     public Film getFilmById(long id);
+
     public Film addFilm(Film newFilm);
+
     public Film updateFilm(Film newFilm);
+
     public Collection<Film> getAllFilms();
 }
--- src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
@@ -6,7 +6,10 @@ import java.util.Collection;
 
 public interface UserStorage {
     public User getUserById(long id);
+
     public User addUser(User newUser);
+
     public User updateUser(User newUser);
+
     public Collection<User> getAllUsers();
 }
