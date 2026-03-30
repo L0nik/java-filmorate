be3d65651f9b964c826c2f774f8f79662b2f6b9a
l0nik <l0nik@mail.ru>
2026-03-30 20:58:04 +0300

    тесты для dbUserStorage

--- src/main/java/ru/yandex/practicum/filmorate/storage/DbUserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/DbUserStorage.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Repository;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.model.User;
 
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Optional;
 
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
-MERGE INTO rating_mpa (id, name) KEY(id) VALUES (1, 'G');
-MERGE INTO rating_mpa (id, name) KEY(id) VALUES (2, 'PG');
-MERGE INTO rating_mpa (id, name) KEY(id) VALUES (3, 'PG-13');
-MERGE INTO rating_mpa (id, name) KEY(id) VALUES (4, 'R');
-MERGE INTO rating_mpa (id, name) KEY(id) VALUES (5, 'NC-17');
+MERGE INTO "rating_mpa" ("id", "name") KEY("id") VALUES (1, 'G');
+MERGE INTO "rating_mpa" ("id", "name") KEY("id") VALUES (2, 'PG');
+MERGE INTO "rating_mpa" ("id", "name") KEY("id") VALUES (3, 'PG-13');
+MERGE INTO "rating_mpa" ("id", "name") KEY("id") VALUES (4, 'R');
+MERGE INTO "rating_mpa" ("id", "name") KEY("id") VALUES (5, 'NC-17');
